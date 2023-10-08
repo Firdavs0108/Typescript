019a19260df5d6f2b4dcfa24c6acf762f4e3a46b
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-10-08 15:58:27 +0900

    basic_TS

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-var fullName = 'Firdavs';
-console.log(fullName);
-var surnameMn = 'Nazarov';
-console.log(surnameMn);
+// const fullName = 'Firdavs'
+// console.log(fullName);
+// const surnameMn: string = 'Nazarov'
+// console.log(surnameMn);
+//array
+var colors = ['red', 'black', 'white'];
+//any
+var smth = 1;
+console.log(smth);
+smth = 'Helen';
+console.log(smth);
--- script.ts
+++ script.ts
@@ -1,7 +1,26 @@
-const fullName = 'Firdavs'
+// const fullName = 'Firdavs'
 
-console.log(fullName);
+// console.log(fullName);
 
-const surnameMn: string = 'Nazarov'
-console.log(surnameMn);
+// const surnameMn: string = 'Nazarov'
+// console.log(surnameMn);
+
+
+//array
+
+const colors: string[] = ['red', 'black', 'white']
+
+//any
+
+// let smth: any = 1
+// console.log(smth);
+
+// smth='Helen'
+
+// console.log(smth);
+
+
+
+//Functions
+
 
