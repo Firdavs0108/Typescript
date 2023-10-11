63e8f04b172b464a2caeae6a06ba2635985458df
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-10-11 21:55:58 +0900

    basic_TS

--- basic/lesson_5/script5.js
+++ basic/lesson_5/script5.js
@@ -1,1 +1,29 @@
 // Literal type
+//  type actionType = 'show' | 'hide';
+//  function logger(action: actionType): boolean {
+//     switch(action){
+//         case 'hide' :
+//          return false;
+//         case 'show' :
+//          return true
+//     }
+//  }
+//  console.log(logger);
+// ENUM///////////
+var Dictionary;
+(function (Dictionary) {
+    Dictionary[Dictionary["rus"] = 0] = "rus";
+    Dictionary[Dictionary["uzb"] = 1] = "uzb";
+    Dictionary[Dictionary["eng"] = 2] = "eng";
+})(Dictionary || (Dictionary = {}));
+var Decision;
+(function (Decision) {
+    Decision[Decision["yes"] = 1] = "yes";
+    Decision["no"] = "no";
+})(Decision || (Decision = {}));
+var uzbIndex = Dictionary.uzb;
+var uzbK = Dictionary[uzbIndex];
+console.log(uzbK);
+// Tuples = Array
+var num = [1, '2', 2];
+console.log(num);
--- basic/lesson_5/script5.ts
+++ basic/lesson_5/script5.ts
@@ -1,3 +1,46 @@
 
 // Literal type
 
+//  type actionType = 'show' | 'hide';
+
+//  function logger(action: actionType): boolean {
+//     switch(action){
+//         case 'hide' :
+//          return false;
+//         case 'show' :
+//          return true
+//     }
+//  }
+
+//  console.log(logger);
+
+
+
+
+// ENUM///////////
+
+enum Dictionary {
+    rus,
+    uzb,
+    eng,
+}
+
+enum Decision {
+    yes = 1,
+    no = 'no'
+}
+
+
+const uzbIndex = Dictionary.uzb;
+const uzbK = Dictionary[uzbIndex]
+
+console.log(uzbK);
+
+
+
+// Tuples = Array
+const num : [number,string,number] = [1,'2', 2]
+
+console.log(num);
+
+ 
