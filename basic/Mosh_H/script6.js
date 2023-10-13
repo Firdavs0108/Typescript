29ec1b50e710911a968b828d92cb97fe04c20154
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-10-13 20:04:50 +0900

    basic_TS

--- basic/Mosh_H/script6.js
+++ basic/Mosh_H/script6.js
@@ -1,8 +1,9 @@
-var Size;
-(function (Size) {
-    Size[Size["small"] = 1] = "small";
-    Size[Size["medium"] = 2] = "medium";
-    Size[Size["large"] = 3] = "large";
-})(Size || (Size = {}));
-var mySize = Size.medium;
-console.log(mySize);
+// // ENUM/////
+var employee = {
+    id: 1,
+    name: 'Fredrick',
+    retire: function (date) {
+        console.log(date);
+    }
+};
+console.log(employee);
--- basic/Mosh_H/script6.ts
+++ basic/Mosh_H/script6.ts
@@ -1,5 +1,44 @@
+// // ENUM/////
 
-enum Size {small = 1, medium, large}
+
+// enum Size {small = 1, medium, large}
  
-let mySize: Size = Size.medium
-console.log(mySize);
+// let mySize: Size = Size.medium
+// console.log(mySize);
+
+
+
+// // FUNCTION/////////
+
+// function calculateTax (income: number, taxYear = 2021) : number {
+//     if(taxYear < 2022)
+//         return income * 2.3;
+//     return income * 3.2
+// }
+
+// calculateTax(10.000)
+// console.log(calculateTax(10.000));
+
+
+// TYPE ALIASES
+
+type Employee = {
+    readonly id: number;
+    name: string;
+    retire: (date: Date)=> void
+}
+
+
+let employee: Employee = {
+    id: 1,
+    name: 'Fredrick',
+    retire: (date: Date)=>{
+        console.log(date);
+        
+    }
+
+}
+
+console.log(employee);
+
+
